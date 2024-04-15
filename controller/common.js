e4f74a6d3560c2322dd4a845901bbb08dfd81a25
Mikhiel <mikhielbenji9@gmail.com>
2024-04-15 10:49:55 +0530

    Added statistics on home page api

--- controller/common.js
+++ controller/common.js
@@ -1,20 +1,34 @@
-const TnpCordinator = require("../models/TnpCordinator");
+const TnpCoordinator = require("../models/TnpCordinator");
 const Company = require("../models/Company");
 const Alumni = require("../models/Alumni");
 const Announcement=require("../models/Announcement");
 
 async function getAll(req, res) {
   try {
-    const announcement = await Announcement.find(); 
-    const company = await Company.find();
+    const announcements = await Announcement.find();
+    const companies = await Company.find();
+    let avgPackage = 0,
+      totalCompanies = 0,
+      totalStudents = 0;
+    for (let i = 0; i < companies.length; i++) {
+      avgPackage += companies[i].avgPackage;
+      totalCompanies++;
+      totalStudents += companies[i].numberOfStudentsPlaced;
+    }
+    const stat = {
+      avgPackage: avgPackage / totalStudents,
+      totalCompanies: totalCompanies,
+      totalStudents: totalStudents,
+    };
     const alumni = await Alumni.find();
-    const tnp = await TnpCordinator.find();
-    res.status(200).json({ announcement, company, alumni, tnp });
+    const tnp = await TnpCoordinator.find();
+    res.status(200).json({ announcements, companies, stat, alumni, tnp });
   } catch (error) {
     console.error("Error fetching data:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 }
+
 
 module.exports = {
   getAll,
